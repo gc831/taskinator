1e500d19c5574e5b1301e031982a09a5be6d14a1
Gio <88457455+gc831@users.noreply.github.com>
2022-03-24 18:39:20 -0500

    code refactor

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,14 +10,20 @@ var taskFormHandler = function(event) {
   var taskNameInput = document.querySelector("input[name='task-name']").value;
   var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-// check if input values are empty strings
+// check if input values are empty strings. Same as if (taskNameInput === "" || taskTypeInput === "")
 if (!taskNameInput || !taskTypeInput) {
   alert("You need to fill out the task form!");
   return false;
 }
 formEl.reset();
+
+// reset form fields for next task to be entered
+document.querySelector("input[name='task-name']").value = "";
+document.querySelector("select[name='task-type']").selectedIndex = 0;
+
+
 // package up data as an object
-  var taskDataObj = {
+var taskDataObj = {
   name: taskNameInput,
   type: taskTypeInput,
   };
@@ -41,8 +47,8 @@ var createTaskEl = function(taskDataObj) {
  
   listItemEl.appendChild(taskInfoEl);
 
-// add entire list item to list
+// add entire list item to list. tasksToDoEl is the child
   tasksToDoEl.appendChild(listItemEl);
 };
-
+// 
   formEl.addEventListener("submit", taskFormHandler);
